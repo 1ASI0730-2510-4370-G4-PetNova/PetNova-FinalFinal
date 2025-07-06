a522a9fc0e1ca2dab73313f6b68bc745585f197f
devfab17 <u202220219@upc.edu.pe>
2025-07-05 23:44:56 -0500

    fix Backend: Delete one BContext

--- PetNova.API/obj/Debug/net9.0/PetNova.API.AssemblyInfo.cs
+++ PetNova.API/obj/Debug/net9.0/PetNova.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetNova.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31fc895f3a9f237a365df6df6df01a993f1c11cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d9a28b5d5560b0f76c191d0283258f6a02794e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetNova.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetNova.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
